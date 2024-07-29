1f851a4927d1a55ce11c2cae3d8abc9e643d45fa
Shelyn Del Mundo <dm.shelyn@gmail.com>
2024-07-28 22:07:22 -0600

    added search bar to map and InAppLoago fixed

--- src/components/inAppLogo.js
+++ src/components/inAppLogo.js
@@ -3,7 +3,7 @@ import AppLogo from "../assets/SocialGrdnInAppLogo.png";
 
 export default function InAppLogo() {
     return (
-        <header className='py-2 block w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 top-0 left-0'>
+        <header className='py-2 fixed block w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 top-0 left-0'>
             <img src={AppLogo} alt="Social Grdn Logo" className="w-auto h-auto"/>
             
         </header>
--- src/pages/MapSearch.js
+++ src/pages/MapSearch.js
@@ -2,13 +2,14 @@ import React from "react";
 import InAppLogo from "../components/inAppLogo";
 import NavBar from "../components/navbar";
 import Sprout from "../assets/sprout.png";
+import SearchBar from "../components/searchbar";
 
 export default function MapSearch() {
     return (
         <div className='bg-main-background relative'>
             <InAppLogo />
             <div className="flex flex-col items-center justify-center gap-2 min-h-screen m-2 pb-20">
-
+                <SearchBar />
                 Hello, maps!
 
             </div>
