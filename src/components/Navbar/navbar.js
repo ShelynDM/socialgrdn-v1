da74ba68527d2c6a937df138bba468674f4f2231
tianajb <kriz_bautista@yahoo.com>
2024-09-03 13:25:51 -0600

    reservation update

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -5,33 +5,33 @@ import { FaRegUser } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
 // This is the navigation bar that will be displayed at the bottom of the screen
-export default function NavBar({SproutPath, SearchColor, EarthColor, ProfileColor}) {
+export default function NavBar({ SproutPath, SearchColor, EarthColor, ProfileColor }) {
     return (
         <nav className='fixed bottom-0 w-full border border-t-2 py-4 px-6 pb-5 bg-main-background'>
             <ul className='flex flex-row justify-between'>
                 <li>
                     <Link to="/Search">
-                        <IoSearchSharp className="w-8 h-8 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14" style={{ color: SearchColor }}/>
+                        <IoSearchSharp className="w-8 h-8 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14" style={{ color: SearchColor }} />
                     </Link>
                 </li>
                 <li>
-                    <Link to="/Listing">
-                        <img src={SproutPath} alt="Sprout Icon" className="w-8 h-8 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14"/>
+                    <Link to="/Reservations">
+                        <img src={SproutPath} alt="Sprout Icon" className="w-8 h-8 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14" />
                     </Link>
                 </li>
                 <li>
                     <Link to="/MapSearch">
-                        <FaEarthAmericas className="w-8 h-8 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14" style={{ color: EarthColor }}/>
+                        <FaEarthAmericas className="w-8 h-8 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14" style={{ color: EarthColor }} />
                     </Link>
                 </li>
                 <li>
                     <Link to="/Profile">
-                        <FaRegUser className="w-8 h-8 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14" style={{ color: ProfileColor }}/>   
+                        <FaRegUser className="w-8 h-8 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14" style={{ color: ProfileColor }} />
                     </Link>
                 </li>
             </ul>
         </nav>
-        
-        
+
+
     );
 };
