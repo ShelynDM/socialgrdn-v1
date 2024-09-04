83e481c25fb6c896dbaa2b468006f2a0fd0c4905
tianajb <kriz_bautista@yahoo.com>
2024-09-03 22:02:03 -0600

    minor revision

--- src/pages/ReservationSection/ReservationCancelled.js
+++ src/pages/ReservationSection/ReservationCancelled.js
@@ -5,30 +5,30 @@ import NavBar from "../../components/Navbar/navbar";
 import GreenSprout from "../../assets/navbarAssets/sproutGreen.png";
 
 export default function ReservationCancelled() {
-
     const navigate = useNavigate();
 
     const handleBackToReservations = () => {
         navigate('/Reservations');
     };
 
-
     return (
         <div className='bg-main-background relative'>
             <InAppLogo />
 
             <div className="flex flex-col justify-center gap-2 min-h-screen pb-20 pt-10">
+                <div className="text-center">
+                    <p className="text-2xl font-bold">Your cancellation</p>
+                    <p className="text-2xl font-bold">confirmation</p>
 
+                    <p className="pt-5">Your reservation</p>
+                    <p>has been cancelled</p>
 
-                <div className="">
-                    <p className="text-2xl font-bold  text-center">Your cancellation</p>
-                    <p className="text-2xl font-bold  text-center">confirmation</p>
-
-                    <p className="pt-5 text-center">Your reservation </p>
-                    <p className="text-center">has been cancelled</p>
-
-                    <button className="mt-5 text-teal-600 text-xl font-bold text-center"
-                        onClick={handleBackToReservations}>Back to My Reservation</button>
+                    <button
+                        className="mt-5 text-teal-600 text-xl font-bold block mx-auto"
+                        onClick={handleBackToReservations}
+                    >
+                        Back to My Reservation
+                    </button>
                 </div>
             </div>
             <NavBar SproutPath={GreenSprout} />
