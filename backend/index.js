e67b4f312063aab9b401d0d5c5f061ff76c57a37
2DL3 <donalduy026@yahoo.com>
2024-09-11 10:45:19 -0600

    Partially completed ViewMyListings
    Partially completed ViewMyListings
    
    Need to work on the images

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-// backend/index.js
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
@@ -10,6 +9,7 @@ const userRoutes = require('./api/RegisterAPI');
 const getIDRoutes = require('./api/GetuserIDAPI');
 const editProfileRoutes = require('./api/EditProfileAPI');
 const getProfileRoutes = require('./api/GetUserProfileAPI');
+const getUserPropertiesRoutes = require('./api/GetUserPropAPI'); // Import your new GetUserPropAPI
 
 const app = express();
 const port = 3000;
@@ -27,11 +27,12 @@ app.use('/api/users', userRoutes);
 app.use('/api/profile', getIDRoutes);
 app.use('/api/editProfile', editProfileRoutes);
 app.use('/api/getProfile', getProfileRoutes);
+app.use('/api/getUserProperties', getUserPropertiesRoutes); // Add the new route for GetUserPropAPI
+
 // The "catchall" handler
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
-
 // Server and database setup
 app.listen(port, (err) => {
   if (err) {
