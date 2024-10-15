583d3b962baed0b7ab5d5c8fb7aaf04e7f8fcdb6
lilian-su <liliank.497@gmail.com>
2024-10-15 16:25:25 -0600

    ModeratorViewProfile and Sign in

--- src/UserContext.js
+++ src/UserContext.js
@@ -4,6 +4,7 @@ const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [userId, setUserId] = useState(null);
+  const [role, setRole] = useState(null);
 
   useEffect(() => {
     // On app load, check if the user ID is in localStorage
@@ -18,8 +19,21 @@ export const UserProvider = ({ children }) => {
     localStorage.setItem('userId', id);
   };
 
+  useEffect(() => {
+    // On app load, check if the user role is in localStorage
+    const storedRole = localStorage.getItem('role');
+    if (storedRole) {
+      setRole(storedRole);
+    }
+  }, []);
+
+  const updateRole = (userRole) => {
+    setRole(userRole);
+    localStorage.setItem('role', userRole);
+  }; // Close updateRole function
+
   return (
-    <UserContext.Provider value={{ userId, setUserId: updateUserId }}>
+    <UserContext.Provider value={{ userId, setUserId: updateUserId, role, setRole: updateRole }}>
       {children}
     </UserContext.Provider>
   );
